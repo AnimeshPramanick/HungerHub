1b0e702b65ac8218d5aac1b496bb1744a45ad52f
Animesh Pramanick <apramanick915@gmail.com>
2025-09-01 15:16:41 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import connectDB from "./config/connectDB.js";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "server is running on port " + PORT });
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
