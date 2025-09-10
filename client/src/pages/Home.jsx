153e6087081bb1ad6a08de8bfb8b154f8c11407d
Avi2k03 <avikundu2003@gmail.com>
2025-09-10 19:34:33 +0530

    g

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div>dskjfhkdshf dsfdsngkjds</div>
-    </div>
+      <div>dskjfhkdsh dsfdsngkjds</div>
+      </div>
   );
 };
 
