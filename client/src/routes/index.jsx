4e994e89a660586fcaab3cff0dfdbe0b5447bcca
bishwarupgoswami <bishwarup95@gmail.com>
2025-09-16 21:12:09 +0530

    updated

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import Signup from "../pages/Signup";
+//import ProfileDashboard from "../pages/ProfileDashboard"; // 👈 import new page
 
 const router = createBrowserRouter([
   {
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
         path: "",
         element: <Home />,
       },
+      {
+        path: "Signup",
+        element: <Signup />
+      },
+      //{
+        //path: "profile", // 👈 new route
+        //element: <ProfileDashboard />,
+      //},
     ],
   },
 ]);
